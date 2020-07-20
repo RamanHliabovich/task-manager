8228bedb97b7a54dfb30ae53241fa980d7715dca
Raman hliabovich <hliaborn@mail.uc.edu>
2020-07-20 17:18:06 -0400

    update

---
+++ src/routers/task.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const router = new express.Router()
+const Task = require('../models/task')
+const User = require('../models/user')
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.patch('/tasks/:id', async (req,res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['completed', 'description']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    console.log(isValidOperation)
+
+    if (!isValidOperation) {
+        res.status(400).send({ error: 'Invalid update' })
+    }
+
+    try {
+        const task = await Task.findById(req.params.id)
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.delete('/tasks/:id', async (req,res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
